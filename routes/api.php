e1c5e6ac78d3a7d1dfff489cd8eb1b5278f2aa49
systemdiaz84 <systemdiaz84@gmail.com>
2023-06-24 10:33:21 -0500

    todo

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::resource('trees', TreeController::class)->names('admin.trees')->middleware('auth:sanctum');
 
