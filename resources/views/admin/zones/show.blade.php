36633254cc83838c520b0a8dd4b9bcb40b938cd5
systemdiaz84 <systemdiaz84@gmail.com>
2023-06-28 15:50:23 -0500

    todo

--- resources/views/admin/zones/show.blade.php
+++ resources/views/admin/zones/show.blade.php
@@ -87,6 +87,17 @@
 
 @stop
 
+
+@if (null !== session('action'))
+    <script>
+        Swal.fire(
+            'Proceso Exitoso',
+            '{{ session('action') }}',
+            'success'
+        )
+    </script>
+@endif
+
 @section('js')
     <script>
         $(document).ready(function() {
@@ -129,76 +140,66 @@
                 },
             });
         });
-    </script>
 
-    @if (null !== session('action'))
-        <script>
-            Swal.fire(
-                'Proceso Exitoso',
-                '{{ session('action') }}',
-                'success'
-            )
-        </script>
-    @endif
-@endsection
-<script>
-    var coordenadas = @json($coords); // Obtener el array de coordenadas desde PHP
+        var coordenadas = @json($coords); // Obtener el array de coordenadas desde PHP
 
-    var currentInfoWindow = null;
+        var currentInfoWindow = null;
 
-    function initMap2() {
-        var perimeterCoords = @json($vertice)
+        function initMap2() {
+            var perimeterCoords = @json($vertice)
 
-        if (Object.keys(perimeterCoords).length === 0) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var lat = position.coords.latitude;
-                var lng = position.coords.longitude;
+            if (Object.keys(perimeterCoords).length === 0) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var lat = position.coords.latitude;
+                    var lng = position.coords.longitude;
 
+                    var mapOptions = {
+                        center: {
+                            lat: lat,
+                            lng: lng
+                        },
+                        zoom: 18
+                    };
+                    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+                })
+            } else {
                 var mapOptions = {
-                    center: {
-                        lat: lat,
-                        lng: lng
-                    },
                     zoom: 18
-                };
+                }
                 var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-            })
-        } else {
-            var mapOptions = {
-                zoom: 18
+                //var perimeterCoords = @json($vertice);
+                // Crea un objeto de polígono con los puntos del perímetro
+                var perimeterPolygon = new google.maps.Polygon({
+                    paths: perimeterCoords,
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35
+                });
+
+                perimeterPolygon.setMap(map);
+
+                var bounds = new google.maps.LatLngBounds();
+
+                // Obtener los límites (bounds) del polígono
+                perimeterPolygon.getPath().forEach(function(coord) {
+                    bounds.extend(coord);
+                });
+
+                // Obtener el centro de los límites (bounds)
+                var centro = bounds.getCenter();
+
+                // Mover el mapa para centrarse en el centro del perímetro
+                map.panTo(centro);
+
+                //});
             }
-            var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-            //var perimeterCoords = @json($vertice);
-            // Crea un objeto de polígono con los puntos del perímetro
-            var perimeterPolygon = new google.maps.Polygon({
-                paths: perimeterCoords,
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35
-            });
-
-            perimeterPolygon.setMap(map);
-
-            var bounds = new google.maps.LatLngBounds();
-
-            // Obtener los límites (bounds) del polígono
-            perimeterPolygon.getPath().forEach(function(coord) {
-                bounds.extend(coord);
-            });
-
-            // Obtener el centro de los límites (bounds)
-            var centro = bounds.getCenter();
-
-            // Mover el mapa para centrarse en el centro del perímetro
-            map.panTo(centro);
-
-            //});
         }
-    }
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initMap2" async
-    defer></script>
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initMap2" async
+        defer></script>
+
+@endsection
