db05e545ceb116e4a049a70096fbba1df4f66120
systemdiaz84 <systemdiaz84@gmail.com>
2023-07-04 12:50:31 -0500

    todo

--- app/Http/Controllers/admin/FamilyController.php
+++ app/Http/Controllers/admin/FamilyController.php
@@ -82,7 +82,6 @@ class FamilyController extends Controller
      */
     public function update(Request $request, $id)
     {
-        return $request;
         $family = Family::find($id);
         $family->update($request->all());
 
