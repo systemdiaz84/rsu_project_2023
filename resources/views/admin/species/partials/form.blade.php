7a46b1033807a674936540f8a667b270dd6508cf
systemdiaz84 <systemdiaz84@gmail.com>
2023-07-04 13:09:30 -0500

    todo

--- resources/views/admin/species/partials/form.blade.php
+++ resources/views/admin/species/partials/form.blade.php
@@ -1,19 +1,27 @@
 <div class="form-group">
     {!! Form::label('family_id', 'Familia') !!}
-    {!! Form::select('family_id', $families, null, ['class'=>'form-control','required']) !!}
+    {!! Form::select('family_id', $families, null, ['class' => 'form-control', 'required']) !!}
 </div>
 
 <div class="form-group">
     {!! Form::label('name', 'Nombre común') !!}
-    {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>'Ingrese el nombre común','required']) !!}
+    {!! Form::text('name', null, [
+        'class' => 'form-control',
+        'placeholder' => 'Ingrese el nombre común',
+        'required',
+    ]) !!}
 </div>
 
 <div class="form-group">
     {!! Form::label('scientific_name', 'Nombre científico') !!}
-    {!! Form::text('scientific_name', null, ['class'=>'form-control','placeholder'=>'Ingrese el nombre científico','required']) !!}
+    {!! Form::text('scientific_name', null, [
+        'class' => 'form-control',
+        'placeholder' => 'Ingrese el nombre científico',
+        'required',
+    ]) !!}
 </div>
 
 <div class="form-group">
     {!! Form::label('description', 'Descripción') !!}
-    {!! Form::textarea('description', null, ['class'=>'form-control','placeholder'=>'Ingrese la descripción']) !!}
+    {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la descripción']) !!}
 </div>
