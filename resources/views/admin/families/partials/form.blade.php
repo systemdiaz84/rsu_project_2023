d9f191275d1b7ecd18bafad67c8f0d74d0170aa4
systemdiaz84 <systemdiaz84@gmail.com>
2023-07-04 17:56:46 -0500

    todo

--- resources/views/admin/families/partials/form.blade.php
+++ resources/views/admin/families/partials/form.blade.php
@@ -7,7 +7,14 @@
     ]) !!}
 </div>
 
-
+<div class="form-group">
+    {!! Form::label('scientific', 'Nombre científico') !!}
+    {!! Form::text('scientific', null, [
+        'class' => 'form-control',
+        'placeholder' => 'Ingrese el nombre científico',
+        'required',
+    ]) !!}
+</div>
 
 <div class="form-group">
     {!! Form::label('description', 'Descripción') !!}
