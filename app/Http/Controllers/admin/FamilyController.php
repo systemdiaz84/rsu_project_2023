d9f0acfc2198dfd77404c238ea8984328db8db86
systemdiaz84 <systemdiaz84@gmail.com>
2023-07-04 13:19:36 -0500

    todo

--- app/Http/Controllers/admin/FamilyController.php
+++ app/Http/Controllers/admin/FamilyController.php
@@ -82,8 +82,9 @@ class FamilyController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $family = Family::find($id);
-        return $family->update($request->all());
+
+        return $request.' ------ '.$family = Family::find($id);
+        $family->update($request->all());
 
         //return view('admin.families.show', compact('family'));
         return Redirect()->route('admin.families.index')->with('action', 'Familia Actualizada');
