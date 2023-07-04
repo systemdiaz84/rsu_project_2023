47eaaf0f96f2c67a578ab0efd220365deb4520e2
systemdiaz84 <systemdiaz84@gmail.com>
2023-07-04 12:38:31 -0500

    todo

--- app/Models/admin/Family.php
+++ app/Models/admin/Family.php
@@ -10,7 +10,7 @@ class Family extends Model
 {
     use HasFactory;
 
-    protected $fillable=['name','description'];
+    protected $guarded=[];
 
     public function species(){
         return $this->hasMany(Specie::class);
