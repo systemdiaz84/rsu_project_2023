36301aeed1e4063ab063314dc783f6c51b1c4916
systemdiaz84 <systemdiaz84@gmail.com>
2023-07-18 17:54:49 -0500

    todo

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,9 @@
+{!! Form::model($user, ['route' => ['admin.users.update', $state], 'method' => 'put']) !!}
+
+@include('admin.users.partials.form')
+
+<button type="submit" class="btn btn-success"><i class="fas fa-save"></i>&nbsp;&nbsp;Actualizar</button>
+
+<button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-window-close"></i>&nbsp;&nbsp;Cerrar</button>
+
+{!! Form::close() !!}
