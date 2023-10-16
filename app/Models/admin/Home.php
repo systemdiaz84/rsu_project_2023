655dedc90ca37fb6dd5c577ea4723789668e9157
TINEO ODIAGA ANDRE JORDI <andre.tineo@usat.edu.pe>
2023-10-16 15:20:58 -0500

    añadiendo models faltantes

---
+++ app/Models/admin/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Home extends Model
+{
+    use HasFactory;
+    protected $guarded=[];
+
+}
---
+++ app/Models/admin/HomeMembers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HomeMembers extends Model
+{
+    use HasFactory;
+    protected $guarded=[];
+
+}
