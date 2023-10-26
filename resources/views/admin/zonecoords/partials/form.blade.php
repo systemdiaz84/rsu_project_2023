d0f6bff50345812899c1335ddca159488a21e4db
TINEO ODIAGA ANDRE JORDI <67377660+andrejordi@users.noreply.github.com>
2023-10-26 18:41:42 -0500

    avande de incorpiracion de multi markers al añadir zona

--- resources/views/admin/zonecoords/partials/form.blade.php
+++ resources/views/admin/zonecoords/partials/form.blade.php
@@ -18,6 +18,8 @@
             'required',
         ]) !!}
     </div>
+    <button id="button" type="button" class="btn btn-success mr-2 my-2" ><i class="fas fa-plus"></i>&nbsp;&nbsp;Añadir</button>
+    <button id="delete" type="button" class="btn my-2" style="background-color: green;"><i class="fas fa-trash"></i>&nbsp;&nbsp;Eliminar</button>
 </div>
 <div id="map2" style="height: 400px;width: 100%;"></div>
 <br>
@@ -54,37 +56,108 @@
             zoom: 18
         };
 
+        var markers = [];
+        var eliminar = false;
+        var ids = 0;
+
         var map = new google.maps.Map(document.getElementById('map2'), mapOptions);
 
-        var marker = new google.maps.Marker({
-            position: {
-                lat: lat,
-                lng: lng
-            },
-            map: map,
-            title: 'Ubicación',
-            draggable: true // Permite arrastrar el marcador
-        });
+
+        // SI SE REQUIERE UN MARCADOR AL INICIAR EL MAPA, ENTONCES SE DEBE ABSTRAER
+        // LA FUNCIÓN QUE OYE EL EVENTO
+        // var marker = new google.maps.Marker({
+        //     position: {
+        //         lat: lat,
+        //         lng: lng
+        //     },
+        //     map: map,
+        //     title: 'Ubicación',
+        //     draggable: true // Permite arrastrar el marcador
+        // });
+        // marker.metadata = { type: "point", id: ids + 1 };
+        // markers.push({ id: ids + 1, lat: lat, lng: lng });
+        // ids += 1;
+
+        // Eliminar un marker:
+        // 1. dar click en el botón
+        // 2. dar click sobre marker a eliminar
+        // OJO: si se quiere eliminar otro marker se deben repetir los pasos
+        $("#delete").click(function () {
+            eliminar = !eliminar;
+            $(this).css('background-color', eliminar ? 'red' : 'green');
+        })
 
         var perimeterCoords = @json($vertice);
-            // Crea un objeto de polígono con los puntos del perímetro
-            var perimeterPolygon = new google.maps.Polygon({
-                paths: perimeterCoords,
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35
-            });
+        console.log(perimeterCoords) // inicializar 
 
-            perimeterPolygon.setMap(map);
-
-        // Actualizar las coordenadas al mover el marcador
-        google.maps.event.addListener(marker, 'dragend', function(event) {
-            var latLng = event.latLng;
-            latInput.value = latLng.lat();
-            lonInput.value = latLng.lng();
+        // Crea un objeto de polígono con los puntos del perímetro
+        var perimeterPolygon = new google.maps.Polygon({
+            paths: perimeterCoords,
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35
         });
+
+
+        $("#button").click(function () {
+            var marker =  new google.maps.Marker({
+                position: { lat: lat, lng: lng },
+                draggable: true
+            });
+            marker.metadata = { type: "point", id: ids + 1 };
+            markers.push({ id: ids + 1, lat: lat, lng: lng });
+            ids += 1;
+
+            google.maps.event.addListener(marker, 'click', function (event) {
+                if (eliminar) {
+
+                    // eliminar marcador
+                    markers = markers.filter(i => i.id != marker.metadata.id);
+                    marker.setMap(null);
+
+                    // eliminar marcador del poligono
+                    perimeterPolygon.setMap(null);
+                    perimeterPolygon.setPath(markers.map(mk => ({ lat: mk.lat, lng: mk.lng })));
+                    perimeterPolygon.setMap(map);
+
+                    eliminar = false;//SI SE REQUIERE SEGUIR ELIMINANDO, ENTONCES REMOVER
+                    $("#delete").css('background-color', 'green');
+                }
+            })
+
+            google.maps.event.addListener(marker, 'dragend', function (event) {
+                var latLng = event.latLng;
+
+                markers = markers.map( mk => ({ 
+                    id: mk.id, 
+                    lat: marker.metadata.id == mk.id ? latLng.lat() : mk.lat, 
+                    lng: marker.metadata.id == mk.id ? latLng.lng() : mk.lng 
+                }))
+
+                perimeterPolygon.setMap(null);
+                perimeterPolygon.setPath(markers.map(mk => ({ lat: mk.lat, lng: mk.lng })));
+                perimeterPolygon.setMap(map);
+
+                // console.log(markers)
+                console.log(markers.map(mk => ({ lat: mk.lat, lng: mk.lng })))
+                
+            });
+            marker.setMap(map);
+            console.log(coordenadas)
+
+
+
+        })
+        // perimeterPolygon.setMap(map);
+
+        // // Actualizar las coordenadas al mover el marcador
+        // google.maps.event.addListener(marker, 'dragend', function(event) {
+        //     var latLng = event.latLng;
+        //     latInput.value = latLng.lat();
+        //     lonInput.value = latLng.lng();
+        // });
     }
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initMap" async defer>
