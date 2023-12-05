e1df110e43caac09727029ce2c0439b12580987b
BeafLee <carlos.chung.754@gmail.com>
2023-12-04 20:20:39 -0500

    datos de familias y especies

---
+++ database/seeders/datos_familia_especies/familias.php
@@ -0,0 +1,99 @@
+<?php
+
+return [
+    [
+        'name' => 'Rosáceas',
+        'scientific_name' => 'Rosaceae',
+        'description' => 'Familia de árboles con flores.',
+    ],
+    [
+        'name' => 'Fagáceas',
+        'scientific_name' => 'Fagaceae',
+        'description' => 'Familia de árboles de hojas caducas.',
+    ],
+    [
+        'name' => 'Pináceas',
+        'scientific_name' => 'Pinaceae',
+        'description' => 'Familia de árboles de hojas perennes.',
+    ],
+    [
+        'name' => 'Aceráceas',
+        'scientific_name' => 'Aceraceae',
+        'description' => 'Familia de árboles conocidos por sus hojas palmadas.',
+    ],
+    [
+        'name' => 'Juglandáceas',
+        'scientific_name' => 'Juglandaceae',
+        'description' => 'Familia de árboles con frutos en nueces.',
+    ],
+    [
+        'name' => 'Betuláceas',
+        'scientific_name' => 'Betulaceae',
+        'description' => 'Familia de árboles conocidos por sus cortezas.',
+    ],
+    [
+        'name' => 'Salicáceas',
+        'scientific_name' => 'Salicaceae',
+        'description' => 'Familia de árboles y arbustos con hojas simples y alternas.',
+    ],
+    [
+        'name' => 'Myrtáceas',
+        'scientific_name' => 'Myrtaceae',
+        'description' => 'Familia de árboles y arbustos con hojas opuestas.',
+    ],
+    [
+        'name' => 'Arecáceas',
+        'scientific_name' => 'Arecaceae',
+        'description' => 'Familia de palmas y cocoteros.',
+    ],
+    [
+        'name' => 'Cupresáceas',
+        'scientific_name' => 'Cupressaceae',
+        'description' => 'Familia de coníferas que incluye cipreses y secuoyas.',
+    ],
+    [
+        'name' => 'Moráceas',
+        'scientific_name' => 'Moraceae',
+        'description' => 'Familia de árboles y arbustos que incluye higueras y moreras.',
+    ],
+    [
+        'name' => 'Sapotáceas',
+        'scientific_name' => 'Sapotaceae',
+        'description' => 'Familia de árboles tropicales con savia lechosa.',
+    ],
+    [
+        'name' => 'Anacardiáceas',
+        'scientific_name' => 'Anacardiaceae',
+        'description' => 'Familia que incluye mangos, pistachos y poison ivy.',
+    ],
+    [
+        'name' => 'Leguminosas',
+        'scientific_name' => 'Fabaceae',
+        'description' => 'Familia de árboles y arbustos con frutos en vainas.',
+    ],
+    [
+        'name' => 'Santáceas',
+        'scientific_name' => 'Santalaceae',
+        'description' => 'Familia de árboles parasitarios.',
+    ],
+    [
+        'name' => 'Bignonáceas',
+        'scientific_name' => 'Bignoniaceae',
+        'description' => 'Familia de árboles y arbustos, muchos de ellos ornamentales.',
+    ],
+    [
+        'name' => 'Rutáceas',
+        'scientific_name' => 'Rutaceae',
+        'description' => 'Familia de árboles y arbustos que incluye cítricos.',
+    ],
+    [
+        'name' => 'Araliáceas',
+        'scientific_name' => 'Araliaceae',
+        'description' => 'Familia de árboles y arbustos con hojas grandes y compuestas.',
+    ],
+    [
+        'name' => 'Oleáceas',
+        'scientific_name' => 'Oleaceae',
+        'description' => 'Familia que incluye olivos, fresnos y jazmines.',
+    ],
+];
